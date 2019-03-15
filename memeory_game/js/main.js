e53f698fb26ec9e02ddae22bdd6464cc82ea58d1
mingjingtang <tmjcsh@gmail.com>
2019-03-15 01:39:01 -0400

    created game logic third time

--- memeory_game/js/main.js
+++ memeory_game/js/main.js
@@ -1,20 +1,23 @@
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped queen");
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else{
-		alert("Sorry, try again");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardId){
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
