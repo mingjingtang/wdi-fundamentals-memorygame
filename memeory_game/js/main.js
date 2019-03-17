7db7bc939af34a33b01f516e8f19ac25f465d50d
mingjingtang <tmjcsh@gmail.com>
2019-03-17 13:21:29 -0400

    created game logic fourth time

--- memeory_game/js/main.js
+++ memeory_game/js/main.js
@@ -1,5 +1,27 @@
 
-var cards = ["queen","queen","king","king"];
+var cards=[
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank:'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank:'king',
+	suit:'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank:'king',
+	suit:'diamonds',
+	cardImage:'images/king-of-diamonds.png'
+}
+];
+
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -11,7 +33,10 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);s
+	console.log(cards[cardId].suit);
+	
 
 	if(cardsInPlay.length === 2){
 		checkForMatch();
